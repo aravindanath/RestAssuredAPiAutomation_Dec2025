2e5fffa641f7d4a55488d996947dff4cfb58a238
Mamta Pandey <panday63ji@gmail.com>
2025-12-19 20:43:55 +0530

    Assignment

---
+++ src/test/java/day1/AssignmentTC001.java
@@ -0,0 +1,72 @@
+package day1;
+
+import com.google.gson.JsonObject;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+
+import static io.restassured.RestAssured.given;
+
+public class AssignmentTC001 {
+
+    String token;
+
+    @Test(description = "Login Request")
+    public void loginRequest() {
+
+        RestAssured.baseURI = Contants.BASE_URL;
+        RestAssured.basePath = Contants.loginUser;
+
+        JsonObject requestBody = new JsonObject();
+        requestBody.addProperty("email", "stevj@test.com");
+        requestBody.addProperty("password", "Test@1234");
+
+        // Request
+        Response response =  given().contentType(ContentType.JSON)
+                .body(requestBody).log().all().post();
+        System.out.println(response.statusCode());
+        Assert.assertEquals(response.statusCode(),200,"Status code is not 200");
+        token =  response.then().extract().path("token");
+        response.prettyPrint();
+
+
+
+    }
+
+
+    @Test(description = "Add Contacts Request", dependsOnMethods = {"loginRequest"})
+    public void addContactsRequest() {
+
+        RestAssured.baseURI = Contants.BASE_URL;
+        RestAssured.basePath = Contants.contacts;
+
+        HashMap<String, String> path = new HashMap<>();
+        path.put("firstName", "Sakshi");
+        path.put("lastName", "Mishra");
+        path.put("email", "as@test.com");
+        path.put("phone", "1234567890");
+        path.put("street1", "123 Main St");
+        path.put("street2", "Apt 4B");
+        path.put("birthdate", "1970-01-01");
+        path.put("city", "Metropolis");
+        path.put("stateProvince", "State");
+        path.put("postalCode", "221503");
+        path.put("country", "India");
+
+
+        // Request
+        Response response = given().header("Authorization","Bearer "+token).contentType(ContentType.JSON)
+                .body(path).log().all().post();
+
+        response.prettyPrint();
+
+
+
+    }
+}
+
+
